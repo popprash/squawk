c80568b4cd5400dbf77a2c09061dc0026730edd5
popprash <jeezyprash@gmail.com>
2026-07-23 21:15:20 +0530

    feat: implement MongoDB connection, Clerk authentication, and user/message data models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,27 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './lib/db.js'
+import {clerkMiddleware} from '@clerk/express'
+import cors from 'cors' 
+
 
 dotenv.config()
 
 const app = express()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT 
+const allowedOrigins = [process.env.FRONTEND_URL]
 
+app.use(express.json())
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}))
+app.use(clerkMiddleware())
+app.get('/health',(req, res)=>{
+    res.json({message: 'OK'})
+})
 app.listen(PORT, () => {
+    connectDB()
     console.log(`Server is running on port ${PORT}`)
 })
