b88edd30b0455e2a3a3b3befe34de61a26a2ed38
popprash <jeezyprash@gmail.com>
2026-07-24 07:29:57 +0530

    feat: integrate Clerk authentication and implement multi-stage Docker build for static asset serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import connectDB from './lib/db.js'
 import {clerkMiddleware} from '@clerk/express'
 import cors from 'cors' 
+import fs from 'fs'
+import path from 'path'
 
 
 dotenv.config()
@@ -11,6 +13,8 @@ const app = express()
 
 const PORT = process.env.PORT 
 const allowedOrigins = [process.env.FRONTEND_URL]
+
+const publidDir = path.join(process.cwd(),"public")
 
 app.use(express.json())
 app.use(cors({
@@ -21,6 +25,15 @@ app.use(clerkMiddleware())
 app.get('/health',(req, res)=>{
     res.json({message: 'OK'})
 })
+
+// if the publidDir exicts we will serve theme as static filese
+if(fs.existsSync(publidDir)){
+    app.use(express.static(publidDir))
+    app.get('/{*any}',(req, res, next)=>{
+        res.sendFile(path.join(publidDir, "index.html"),(err)=> next(err))
+    })
+}
+
 app.listen(PORT, () => {
     connectDB()
     console.log(`Server is running on port ${PORT}`)
