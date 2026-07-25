292b23275214a6d9762134fe82748749a7512cf8
popprash <jeezyprash@gmail.com>
2026-07-25 11:19:27 +0530

    feat: replace static loading text with dedicated PageLoader component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,12 @@ import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
 import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
   if(!isLoaded) {
-    return <div>Loading...</div>
+    return <PageLoader/>
   }
   return (
     <>
