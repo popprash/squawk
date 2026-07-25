7a7798186f5c531ab3ad6fc828e324b727b6cbdb
popprash <jeezyprash@gmail.com>
2026-07-25 09:28:17 +0530

    feat: implement theme management, HeroUI integration, and asset-rich wallpaper context.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,39 @@
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { Button } from "@heroui/react";
+import { ThemeProvider } from "./context/ThemeContext";
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+  if(!isLoaded) {
+    return <div>Loading...</div>
+  }
   return (
     <>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode='modal'/>
-          <SignUpButton mode='modal' />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
+      <ThemeProvider>
+        <WallpaperProvider>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />
+              }
+            ></Route>
+            <Route
+              path="/auth"
+              element={
+                !isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />
+              }
+            ></Route>
+          </Routes>
+        </WallpaperProvider>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { ClerkProvider } from "@clerk/react";
+import { ClerkProvider , SignInButton , SignUpButton , UserButton , Show} from "@clerk/react";
+import App from "./App";
+import {BrowserRouter} from 'react-router';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
 );
